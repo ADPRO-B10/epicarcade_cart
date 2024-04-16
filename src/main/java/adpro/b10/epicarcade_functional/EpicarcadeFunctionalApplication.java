a9a1b5325c0e0a1f2ece872245386136645b6daf
sorfeb <soros21febriano@gmail.com>
2024-04-16 09:05:26 +0700

    Initialize Project Structure

---
+++ src/main/java/adpro/b10/epicarcade_functional/EpicarcadeFunctionalApplication.java
@@ -0,0 +1,13 @@
+package adpro.b10.epicarcade_functional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicarcadeFunctionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicarcadeFunctionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/adpro/b10/epicarcade_functional/EpicarcadeFunctionalApplicationTests.java
@@ -0,0 +1,13 @@
+package adpro.b10.epicarcade_functional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicarcadeFunctionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
