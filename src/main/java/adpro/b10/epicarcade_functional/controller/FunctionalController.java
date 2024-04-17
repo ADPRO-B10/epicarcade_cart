734692435cd34b5dcede67e74a6d9ebf7e26b352
Winoto Hasyim <winoto.hasyim@ui.ac.id>
2024-04-17 20:35:34 +0700

    Fix whitelabel

--- src/main/java/adpro/b10/epicarcade_functional/controller/FunctionalController.java
+++ src/main/java/adpro/b10/epicarcade_functional/controller/FunctionalController.java
@@ -3,13 +3,13 @@ package adpro.b10.epicarcade_functional.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/")
+@RestController()
+@RequestMapping("")
 public class FunctionalController {
     @GetMapping("/hello")
     public String helloWorld() {
         return "HelloWorld2";
     }
 }
-
