7d1d3dfc8a514dd8c35ee7beb70bfe95a2bdd852
Winoto Hasyim <winoto.hasyim@ui.ac.id>
2024-04-17 19:51:36 +0700

    Fix

---
+++ src/main/java/adpro/b10/controller/FunctionalController.java
@@ -0,0 +1,15 @@
+package adpro.b10.epicarcade_functional.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequestMapping("/")
+public class FunctionalController {
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "HelloWorld";
+    }
+}
+
