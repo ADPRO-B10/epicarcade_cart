e3219065b30176421dacc181ca15cd8ee1225c42
codaaa19 <tsabitiphone@gmail.com>
2024-04-25 11:38:21 +0700

    Adding function in repo

--- src/main/java/adpro/b10/epicarcade_functional/Review/Repository/ReviewRepository.java
+++ src/main/java/adpro/b10/epicarcade_functional/Review/Repository/ReviewRepository.java
@@ -3,5 +3,8 @@ package adpro.b10.epicarcade_functional.Review.Repository;
 import adpro.b10.epicarcade_functional.Review.Model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findAllByGameId(String gameId);
 }
