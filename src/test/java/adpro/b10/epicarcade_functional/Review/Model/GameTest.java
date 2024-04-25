8e15277d6f6c1aa223b0abe569ee610cb8c6f41a
codaaa19 <tsabitiphone@gmail.com>
2024-04-25 09:36:27 +0700

    [GREEN] Implement Stock in Model testing

--- src/test/java/adpro/b10/epicarcade_functional/Review/Model/GameTest.java
+++ src/test/java/adpro/b10/epicarcade_functional/Review/Model/GameTest.java
@@ -10,12 +10,14 @@ public class GameTest {
         String description = "A puzzle game";
         String id1 = "123456789";
         int price = 20000;
+        int stock = 5;
         Game game = new GameBuilder().name(name).description(description).id(id1)
-                .price(price).build();
+                .price(price).stock(stock).build();
 
         assertNotNull(game.getId(), "ID should not be null");
         assertNotNull(game.getName(), "Game name cannot be empty");
         assertEquals(id1, game.getId(), "ID should match the input");
+        assertEquals(stock, game.getStock(), "Stock should match the input");
         assertEquals(name, game.getName(), "Name should match the input");
         assertEquals(description, game.getDescription(), "Description should match the input");
 
@@ -24,7 +26,7 @@ public class GameTest {
     @Test
     public void testSetDescription_NullDescription_ThrowsException() {
         Game game = new GameBuilder().name("Pac-Man").description("Arcade game").id("123456789")
-                .price(20000).build();
+                .price(20000).stock(5).build();
 
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             game.setDescription(null);
@@ -36,7 +38,7 @@ public class GameTest {
     @Test
     public void testSetName_NullName_ThrowsException() {
         Game game = new GameBuilder().name("Pac-Man").description("Arcade game").id("123456789")
-                .price(20000).build();
+                .price(20000).stock(5).build();
 
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             game.setName(null);
