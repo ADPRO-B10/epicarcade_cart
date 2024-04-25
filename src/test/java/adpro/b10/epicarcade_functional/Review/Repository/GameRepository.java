372d13f03b7ef8b0391fa583b2ce0034e3eb94f7
codaaa19 <tsabitiphone@gmail.com>
2024-04-25 09:38:34 +0700

    Fix GameRepo and ReviewRepo place folder

--- src/test/java/adpro/b10/epicarcade_functional/Review/Repository/GameRepository.java
+++
@@ -1,7 +0,0 @@
-package adpro.b10.epicarcade_functional.Review.Repository;
-
-import adpro.b10.epicarcade_functional.Review.Model.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameRepository extends JpaRepository<Game, Long> {
-}
