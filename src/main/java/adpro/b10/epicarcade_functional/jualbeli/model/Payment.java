d6c9963ed1b43c06a4662e5a28915d287dcc9381
Winoto Hasyim <winoto.hasyim@ui.ac.id>
2024-05-01 20:50:01 +0700

    [REFACTOR] Partially Fixed Order and Payment Model

--- src/main/java/adpro/b10/epicarcade_functional/jualbeli/model/Payment.java
+++ src/main/java/adpro/b10/epicarcade_functional/jualbeli/model/Payment.java
@@ -1,29 +1,42 @@
 package adpro.b10.epicarcade_functional.jualbeli.model;
 
-import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.util.Map;
+import javax.persistence.*;
 
 import adpro.b10.epicarcade_functional.jualbeli.enums.PaymentStatus;
 
-@Builder
+@Entity
+@Table(name = "payments")
 @Getter
+@Setter
+@NoArgsConstructor
 public class Payment {
-    String id;
-    String method;
-    String orderId;
-    String status;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
 
-    public Payment(String id, String method, String orderId) {
+    @Column(name = "method")
+    private String method;
+
+    @OneToOne
+    @JoinColumn(name = "order_id", referencedColumnName = "id")
+    private Order order;
+
+    @Column(name = "status")
+    private String status;
+
+    public Payment(String id, String method, Order order) {
         this.id = id;
-        this.orderId = orderId;
+        this.order = order;
         this.method = method;
-        this.status = "PENDING";
+        this.status = PaymentStatus.PENDING.getValue();
     }
 
-    public Payment(String id, String method, String orderId, String status) {
-        this(id, method, orderId);
+    public Payment(String id, String method, Order order, String status) {
+        this(id, method, order);
         this.setStatus(status);
     }
 
@@ -33,5 +46,18 @@ public class Payment {
         } else {
             throw new IllegalArgumentException();
         }
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+        if (order.getPayment() != this) {
+            order.setPayment(this);
+        }
+    }
+
+    public double getAmount() {
+        return order.getGamesQuantity().entrySet().stream()
+            .mapToDouble(entry -> entry.getKey().getPrice() * entry.getValue())
+            .sum();
     }
 }
