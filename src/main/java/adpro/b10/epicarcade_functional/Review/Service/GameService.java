d16c41dd29a4f55fa6e99892c474c529b4d660e0
codaaa19 <tsabitiphone@gmail.com>
2024-05-08 11:32:33 +0700

    [REFACTORING] Using Hardcoding without database to check functionalitu

--- src/main/java/adpro/b10/epicarcade_functional/Review/Service/GameService.java
+++ src/main/java/adpro/b10/epicarcade_functional/Review/Service/GameService.java
@@ -1,17 +1,16 @@
 package adpro.b10.epicarcade_functional.Review.Service;
 
 import adpro.b10.epicarcade_functional.Review.Model.Game;
-import adpro.b10.epicarcade_functional.Review.Repository.GameRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class GameService {
-    @Autowired
-    private GameRepository gameRepository;
-
     public List<Game> getAllGames() {
-        return gameRepository.findAll();
+        return Arrays.asList(
+            new Game("1", "Game One", "First game description", 20, 5),
+            new Game("2", "Game Two", "Second game description", 30, 3)
+        );
     }
 }
