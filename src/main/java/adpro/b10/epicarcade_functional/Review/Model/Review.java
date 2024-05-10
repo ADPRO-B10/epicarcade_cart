4298ef1641058bb3b192bc9eafeff6d6e0e71ffb
codaaa19 <tsabitiphone@gmail.com>
2024-05-10 19:48:41 +0700

    Stil struggling for making the review data works

--- src/main/java/adpro/b10/epicarcade_functional/Review/Model/Review.java
+++ src/main/java/adpro/b10/epicarcade_functional/Review/Model/Review.java
@@ -2,17 +2,20 @@ package adpro.b10.epicarcade_functional.Review.Model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name = "review")
 @Getter
 public class Review {
-
     @Id
-    @Column(name = "idReview", nullable = false, unique = true)
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "idReview", updatable = false, nullable = false, unique = true)
     private String id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_game", nullable = false)
     private Game game;
 
     @Column(name = "rating", nullable = false)
@@ -21,36 +24,26 @@ public class Review {
     @Column(name = "comment", nullable = false)
     private String comment;
 
-    public Review(){
+    public Review() {}
 
-    }
-
-    public Review(String id, Game game, int rating, String comment){
+    public Review(String id, Game game, int rating, String comment) {
         this.id = id;
         this.game = game;
         this.rating = rating;
         this.comment = comment;
     }
 
-    public void setRating(int rating){
-        if(rating == 0){
-            throw new IllegalArgumentException("Rating has to be 1-5");
+    public void setRating(int rating) {
+        if (rating < 1 || rating > 5) {
+            throw new IllegalArgumentException("Rating has to be between 1 and 5");
         }
         this.rating = rating;
     }
 
-    public void setComment(String comment){
-        if(comment == null){
-            throw new IllegalArgumentException("Game comment cannot be empty");
+    public void setComment(String comment) {
+        if (comment == null || comment.trim().isEmpty()) {
+            throw new IllegalArgumentException("Comment cannot be empty");
         }
         this.comment = comment;
     }
-
-    public void setName(Game game, String name){
-        if(name == null){
-            throw new IllegalArgumentException("Game name cannot be empty");
-        }
-        game.setName(name);
-    }
-
 }
