28c97fbe18fd43686455c12bb823ecbf5878a6af
codaaa19 <tsabitiphone@gmail.com>
2024-05-15 20:47:12 +0700

    Implementing Async (belum delete)

--- src/main/java/adpro/b10/epicarcade_functional/Review/Controller/ReviewController.java
+++ src/main/java/adpro/b10/epicarcade_functional/Review/Controller/ReviewController.java
@@ -7,9 +7,8 @@ import adpro.b10.epicarcade_functional.Review.Service.GameService;
 import adpro.b10.epicarcade_functional.Review.Service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/reviews")
@@ -22,17 +21,17 @@ public class ReviewController {
     private ReviewService reviewService;
 
     @GetMapping("/games")
-    public List<Game> listGames() {
+    public CompletableFuture<List<Game>> listGames() {
         return gameService.findAllGames();
     }
 
     @GetMapping("/list-reviews")
-    public List<Review> listReviews() {
+    public CompletableFuture<List<Review>> listReviews() {
         return reviewService.findAllReviews();
     }
 
-@PostMapping("/add-review")
-public Review addReview(@RequestBody AddReviewDTO reviewDTO) {
-    return reviewService.addReview(reviewDTO.getId_game(), reviewDTO.getRating(), reviewDTO.getComment());
+    @PostMapping("/add-review")
+    public CompletableFuture<Review> addReview(@RequestBody AddReviewDTO reviewDTO) {
+        return reviewService.addReview(reviewDTO.getId_game(), reviewDTO.getRating(), reviewDTO.getComment());
     }
 }
--- src/main/java/adpro/b10/epicarcade_functional/Review/Service/ReviewService.java
+++ src/main/java/adpro/b10/epicarcade_functional/Review/Service/ReviewService.java
@@ -2,11 +2,14 @@ package adpro.b10.epicarcade_functional.Review.Service;
 
 import adpro.b10.epicarcade_functional.Review.Model.Game;
 import adpro.b10.epicarcade_functional.Review.Model.Review;
+import adpro.b10.epicarcade_functional.Review.Repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.UUID;
-import adpro.b10.epicarcade_functional.Review.Repository.ReviewRepository;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class ReviewService {
@@ -14,21 +17,21 @@ public class ReviewService {
     @Autowired
     private ReviewRepository reviewRepository;
 
-    public Review addReview(String id_game, int rating, String comment) {
-        Review review = new Review();
-        review.setId(UUID.randomUUID().toString());
-        review.setId_game(id_game);
-        review.setRating(rating);
-        review.setComment(comment);
+    @Async("taskExecutorForHeavyTasks")
+    public CompletableFuture<Review> addReview(String id_game, int rating, String comment) {
+        return CompletableFuture.supplyAsync(() -> {
+            Review review = new Review();
+            review.setId(UUID.randomUUID().toString());
+            review.setId_game(id_game);
+            review.setRating(rating);
+            review.setComment(comment);
 
-        return reviewRepository.save(review);
+            return reviewRepository.save(review);
+        });
     }
 
-//    public List<Review> findReviewsByGame(String Id_game) {
-//        return reviewRepository.findAllById_game(Id_game);
-//    }
-
-    public List<Review> findAllReviews() {
-        return reviewRepository.findAll();
+    @Async("taskExecutorForHeavyTasks")
+    public CompletableFuture<List<Review>> findAllReviews() {
+        return CompletableFuture.supplyAsync(() -> reviewRepository.findAll());
     }
 }
