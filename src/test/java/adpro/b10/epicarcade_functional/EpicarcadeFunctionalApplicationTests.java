8d3e64640ef0fd3398e9ac3edfb466037706098a
Winoto Hasyim <winoto.hasyim@ui.ac.id>
2024-05-27 05:19:03 +0700

    ..

--- src/test/java/adpro/b10/epicarcade_functional/EpicarcadeFunctionalApplicationTests.java
+++ src/test/java/adpro/b10/epicarcade_functional/EpicarcadeFunctionalApplicationTests.java
@@ -3,11 +3,11 @@ package adpro.b10.epicarcade_functional;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
-// class EpicarcadeFunctionalApplicationTests {
+ @SpringBootTest
+ class EpicarcadeFunctionalApplicationTests {
 
-// 	@Test
-// 	void contextLoads() {
-// 	}
+ 	@Test
+ 	void contextLoads() {
+ 	}
 
-// }
+ }
